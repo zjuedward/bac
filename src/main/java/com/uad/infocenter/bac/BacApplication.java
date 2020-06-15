eff33c1b182537b3a5f2e0455af288ff8b6d3169
zjuedward <zhuxiaowei_1024@163.com>
2020-06-15 09:24:17 +0800

    Initial commit

---
+++ src/main/java/com/uad/infocenter/bac/BacApplication.java
@@ -0,0 +1,13 @@
+package com.uad.infocenter.bac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uad/infocenter/bac/BacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uad.infocenter.bac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
